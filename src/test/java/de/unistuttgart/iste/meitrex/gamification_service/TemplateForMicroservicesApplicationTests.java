fd2a908667dc0d869800738f8b7918e3b6c9c430
Aaron Schmid <st166450@stud.uni-stuttgart.de>
2025-05-02 13:02:50 +0200

    initial steps for new service

---
+++ src/test/java/de/unistuttgart/iste/meitrex/gamification_service/TemplateForMicroservicesApplicationTests.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.iste.meitrex.gamification_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateForMicroservicesApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
