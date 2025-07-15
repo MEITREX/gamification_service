fff335822d9e41e00ccd752007b94fb9781b1c06
Aaron Schmid <st166450@stud.uni-stuttgart.de>
2025-07-15 16:03:04 +0200

    add items

---
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/persistence/entity/items/UserEntity.java
@@ -0,0 +1,23 @@
+package de.unistuttgart.iste.meitrex.gamification_service.persistence.entity.items;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserEntity {
+    @Id
+    UUID id;
+
+}
