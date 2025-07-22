f3c4bce9367317f89280ea513b0e4bdc135dffc4
Aaron Schmid <56757286+Aaron2000119@users.noreply.github.com>
2025-07-22 15:02:07 +0200

    add item tests

---
+++ src/test/java/de/unistuttgart/iste/meitrex/gamification_service/service/ItemServiceTest.java
@@ -0,0 +1,30 @@
+package de.unistuttgart.iste.meitrex.gamification_service.service;
+
+import de.unistuttgart.iste.meitrex.gamification_service.model.ColorTheme;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.MockitoAnnotations.openMocks;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ItemServiceTest {
+
+    ItemService itemService;
+
+    @BeforeEach
+    void setUp() {
+        openMocks(this);
+        itemService = new ItemService();
+    }
+
+    @Test
+    void testStartup() {
+        List<ColorTheme> colorThemeList = itemService.getColorThemes();
+        assertThat(colorThemeList, hasSize(20));
+    }
+}
