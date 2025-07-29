7938183b4dee6876ae04d25732f104dbb385cf8a
Aaron Schmid <56757286+Aaron2000119@users.noreply.github.com>
2025-07-29 13:30:39 +0200

    remove mock

--- src/main/java/de/unistuttgart/iste/meitrex/gamification_service/controller/ItemController.java
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/controller/ItemController.java
@@ -36,28 +36,6 @@ public class ItemController {
     @MutationMapping
     public Inventory buyItem(@Argument UUID itemId,
                              @ContextValue final LoggedInUser loggedInUser) {
-        UserItem item1 = UserItem.builder()
-                .setEquipped(true)
-                .setId(UUID.fromString("1f1c362f-ff7c-490a-82fa-e478510bcf41"))
-                .setUnlocked(true)
-                .setUniqueDescription("Unique description")
-                .build();
-        UserItem item2 = UserItem.builder()
-                .setEquipped(false)
-                .setId(UUID.fromString("6fb8b726-2db2-4992-9e63-f3aa57aa4520"))
-                .setUnlocked(false)
-                .setUniqueDescription("Unique description two")
-                .build();
-        UserItem item3 = UserItem.builder()
-                .setEquipped(false)
-                .setId(itemId)
-                .setUnlocked(true)
-                .setUniqueDescription("Unique description bought")
-                .build();
-        return Inventory.builder()
-                .setUserId(loggedInUser.getId())
-                .setItems(List.of(item1, item2, item3))
-                .setUnspentPoints(30)
-                .build();
+        return new Inventory();
     }
 }
