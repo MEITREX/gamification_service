d5ef264b5c70f707ed9c772661de1e19e7dde64b
Philipp Kunz <philipp_kunz@outlook.com>
2025-09-05 12:02:03 +0200

    Cleanup

--- src/main/java/de/unistuttgart/iste/meitrex/gamification_service/service/DefaultLeaderboardService.java
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/service/DefaultLeaderboardService.java
@@ -27,9 +27,6 @@ import de.unistuttgart.iste.meitrex.gamification_service.events.internal.*;
 @Transactional
 class DefaultLeaderboardService implements ILeaderboardService {
 
-    private static final String ERR_MSG_NO_SUCH_PERSISTENT_USER_PROGRESS_UPDATED_EVENT
-            = "Can not process user progress updated event since its persistent peer is missing.";
-
     private static final String ERR_MSG_ILLEGAL_ATTEMPT_OF_FUTURE_LEADERBOARD_CREATION
             = "Can not create a new leader board starting in the future.";
 
@@ -68,22 +65,13 @@ class DefaultLeaderboardService implements ILeaderboardService {
         return newLeaderboard;
     }
 
-    // Service
-
-    private final DefaultUserService userService;
-
-    private final DefaultCourseService courseService;
-
 
     // Repositories
-
-    private final IUserScoreRepository userScoreRepository;
 
     private final ILeaderboardRepository leaderboardRepository;
 
     private final ICourseRepository courseRepository;
 
-    private final IPersistentUserProgressUpdatedRepository userProgressUpdatedRepository;
 
     //Mapping
 
@@ -91,35 +79,22 @@ class DefaultLeaderboardService implements ILeaderboardService {
 
     private final int dtoRecursionDepth;
 
-    // Functional
-
-    private final IScoringFunction scoringFunction;
 
     private final IPeriodCalculator periodCalculator;
 
     private final ITimeService timeService;
 
     public DefaultLeaderboardService(
-            @Autowired IScoringFunction scoringFunction,
-            @Autowired IUserScoreRepository userScoreRepository,
             @Autowired ILeaderboardRepository leaderboardRepository,
             @Autowired ICourseRepository courseRepository,
-            @Autowired DefaultUserService userService,
-            @Autowired DefaultCourseService courseService,
-            @Autowired IPersistentUserProgressUpdatedRepository userProgressUpdatedRepository,
             @Autowired IPeriodCalculator periodCalculator,
             @Autowired ITimeService timeService,
             @Autowired LeaderboardMapper leaderboardMapper,
             @Value("${de.unistuttgart.iste.meitrex.gamification_service.service.dtoRecursionDepth: 3}")
             int dtoRecursionDepth
     ) {
-        this.scoringFunction = Objects.requireNonNull(scoringFunction);
-        this.userScoreRepository = Objects.requireNonNull(userScoreRepository);
         this.leaderboardRepository = Objects.requireNonNull(leaderboardRepository);
         this.courseRepository = Objects.requireNonNull(courseRepository);
-        this.userService = Objects.requireNonNull(userService);
-        this.courseService = Objects.requireNonNull(courseService);
-        this.userProgressUpdatedRepository = Objects.requireNonNull(userProgressUpdatedRepository);
         this.periodCalculator = Objects.requireNonNull(periodCalculator);
         this.timeService = Objects.requireNonNull(timeService);
         this.leaderboardMapper = Objects.requireNonNull(leaderboardMapper);
@@ -183,102 +158,4 @@ class DefaultLeaderboardService implements ILeaderboardService {
         newLeaderboard = this.leaderboardRepository.save(newLeaderboard);
         updateCourseLeaderboard(courseEntity, newLeaderboard, now, period);
     }
-
-
-    //Logic handling user progress
-/*
-    @Transactional
-    @EventListener
-    public void onUserProgressUpdated(InternalUserProgressUpdatedEvent event) {
-        final UUID id = Objects.requireNonNull(event.getId());
-        final PersistentUserProgressUpdatedEvent persistentEvent
-                = this.userProgressUpdatedRepository
-                .findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(ERR_MSG_NO_SUCH_PERSISTENT_USER_PROGRESS_UPDATED_EVENT));
-        processPersistentUserProgressUpdatedEvent(persistentEvent);
-    }
-
-    @Transactional
-    @Scheduled(fixedRate = 5000)
-    public void retryFailedPersistentUserProgressUpdatedEventsProcessing() {
-        this.userProgressUpdatedRepository
-                .findByStatus(PersistentEvent.Status.FAILED_RETRY)
-                .forEach(this::processPersistentUserProgressUpdatedEvent);
-    }
-
-
-    private void processPersistentUserProgressUpdatedEvent(PersistentUserProgressUpdatedEvent event) {
-
-        final long now = this.timeService.curTime();
-        final LocalDate today = this.timeService.toDate();
-
-        final UserEntity userEntity = this.userService.fetchOrCreate(event.getUserId());
-        final CourseEntity courseEntity = this.courseService.fetchOrCreate(event.getCourseId());
-
-        this.leaderboardRepository.findByCourseAndPeriodOrderByStartDateDesc(courseEntity, Period.WEEKLY);
-
-        final List<LeaderboardEntity> leaderboardEntityList = Arrays.stream(Period.values())
-                .map(period ->  this.leaderboardRepository.findByCourseAndPeriodOrderByStartDateDesc(courseEntity, period))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .toList();
-
-
-
-        if(leaderboardEntityList.size() < Period.values().length || checkIfLeaderboardIsOutdated(leaderboardEntityList, today)) {
-            this.handleUnmetDependency(event, now);
-            return;
-        }
-
-        this.updateUserScoreEntity(leaderboardEntityList, userEntity, courseEntity, event);
-
-        event.setStatus(PersistentEvent.Status.PROCESSED);
-
-    }
-
-
-    private boolean checkIfLeaderboardIsOutdated(List<LeaderboardEntity> leaderboardEntityList, LocalDate today) {
-        for(LeaderboardEntity leaderboardEntity : leaderboardEntityList) {
-            if(!Period.ALL_TIME.equals(leaderboardEntity.getPeriod())){
-                final LocalDate curStartDate = leaderboardEntity.getStartDate();
-                final LocalDate nextStartDate = this.periodCalculator.calcSucStartDate(curStartDate, leaderboardEntity.getPeriod());
-                if(!isDayInRange(today, curStartDate, nextStartDate)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private void handleUnmetDependency(PersistentUserProgressUpdatedEvent event, Long lastProcessingTimestamp) {
-        int newCount = event.getAttemptCount() + 1;
-        event.setAttemptCount(newCount);
-        event.setLastProcessingAttemptTimestamp(lastProcessingTimestamp);
-        PersistentEvent.Status newStatus = newCount >= event.getMaxCount() ? PersistentEvent.Status.FAILED : PersistentEvent.Status.FAILED_RETRY;
-        event.setStatus(newStatus);
-    }
-
-
-    private void updateUserScoreEntity(List<LeaderboardEntity> leaderboardEntityList, UserEntity userEntity, CourseEntity courseEntity, PersistentUserProgressUpdatedEvent event) {
-        leaderboardEntityList.forEach(leaderboardEntity -> {
-            final UserScoreEntity userScoreEntity = this.findOrCreateMostRecentUserScoreEntity(leaderboardEntity, userEntity, courseEntity, leaderboardEntity.getPeriod());
-            final double curScore = userScoreEntity.getScore();
-            final double newAdditionalScore = this.scoringFunction.score(event.getCorrectness(), event.getAssessmentAttempt());
-            userScoreEntity.setScore(curScore + newAdditionalScore);
-        });
-    }
-
-    private UserScoreEntity findOrCreateMostRecentUserScoreEntity(LeaderboardEntity leaderboardEntity, UserEntity user, CourseEntity course, Period period) {
-        return this.userScoreRepository
-                .findMostRecentUserScore(user, course, period)
-                .orElseGet(() -> {
-                    final UserScoreEntity scoreEntity = new UserScoreEntity();
-                    scoreEntity.setLeaderboard(leaderboardEntity);
-                    leaderboardEntity.getScoreEntityList().add(scoreEntity);
-                    scoreEntity.setUser(user);
-                    scoreEntity.setScore(0.0);
-                    return this.userScoreRepository.save(scoreEntity);
-                });
-    }
-    */
 }
