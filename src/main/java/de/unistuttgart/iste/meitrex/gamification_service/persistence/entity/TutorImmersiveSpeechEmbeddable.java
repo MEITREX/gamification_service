25eed1b9ab5a94593d4109261d893020f3b83f58
myluki2000 <lukas636@live.com>
2025-09-09 15:11:49 +0200

    Prevent tutor speeches of over 255 chars in length from crashing

--- src/main/java/de/unistuttgart/iste/meitrex/gamification_service/persistence/entity/TutorImmersiveSpeechEmbeddable.java
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/persistence/entity/TutorImmersiveSpeechEmbeddable.java
@@ -1,5 +1,6 @@
 package de.unistuttgart.iste.meitrex.gamification_service.persistence.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
@@ -8,6 +9,8 @@ import java.time.OffsetDateTime;
 @Embeddable
 @Data
 public class TutorImmersiveSpeechEmbeddable {
+    @Column(columnDefinition = "TEXT")
     private String recentActivitiesString;
+    @Column(columnDefinition = "TEXT")
     private String tutorSpeechContent;
 }
