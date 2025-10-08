9a9cd8f16ab90d662ad17ac9b2103b93af2b696d
Philipp Kunz <philipp_kunz@outlook.com>
2025-10-08 15:01:02 +0200

    Restructured User type to avoid conflicting IDs with UserInfo as managed by the UserService.

--- src/main/java/de/unistuttgart/iste/meitrex/gamification_service/persistence/mapper/UserMapper.java
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/persistence/mapper/UserMapper.java
@@ -9,6 +9,7 @@ import de.unistuttgart.iste.meitrex.generated.dto.*;
 import de.unistuttgart.iste.meitrex.gamification_service.persistence.entity.*;
 
 import java.util.Objects;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +20,9 @@ public class UserMapper {
             return null;
         }
         final User user = new User();
-        user.setId(userEntity.getId());
+        //TODO Id should store the actual user id.
+        user.setId(UUID.randomUUID());
+        user.setRefUserID(userEntity.getId());
         final Integer xpValue = userEntity.getXpValue();
         final Double requiredXP = userEntity.getRequiredXP();
         final Double exceedingXP = userEntity.getExceedingXP();
