3508d5a444cfd026a684663914ababe2df9c15a5
Aaron Schmid <st166450@stud.uni-stuttgart.de>
2025-10-08 16:38:57 +0200

    fix xp

--- src/main/java/de/unistuttgart/iste/meitrex/gamification_service/service/reactive/items/InternalSubmissionCompletedEventListener.java
+++ src/main/java/de/unistuttgart/iste/meitrex/gamification_service/service/reactive/items/InternalSubmissionCompletedEventListener.java
@@ -7,7 +7,10 @@ import de.unistuttgart.iste.meitrex.gamification_service.events.internal.Transie
 import de.unistuttgart.iste.meitrex.gamification_service.events.persistent.PersistentSubmissionCompletedEvent;
 import de.unistuttgart.iste.meitrex.gamification_service.events.repository.IPersistentEventRepository;
 import de.unistuttgart.iste.meitrex.gamification_service.events.repository.IPersistentEventStatusRepository;
+import de.unistuttgart.iste.meitrex.gamification_service.persistence.entity.UserEntity;
 import de.unistuttgart.iste.meitrex.gamification_service.service.IItemService;
+import de.unistuttgart.iste.meitrex.gamification_service.service.internal.IUserCreator;
+import de.unistuttgart.iste.meitrex.gamification_service.service.internal.IUserXPAdder;
 import de.unistuttgart.iste.meitrex.gamification_service.time.ITimeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,14 +25,22 @@ public class InternalSubmissionCompletedEventListener extends AbstractInternalLi
 
     private final IItemService itemService;
 
+    private final IUserCreator userCreator;
+
+    private final IUserXPAdder userXPAdder;
+
     private static final String name = "InternalSubmissionCompletedEventListener";
 
     public InternalSubmissionCompletedEventListener(@Autowired IPersistentEventRepository<PersistentSubmissionCompletedEvent> persistentEventRepository,
                                                     @Autowired IPersistentEventStatusRepository eventStatusRepository,
                                                     @Autowired ITimeService timeService,
-                                                    @Autowired IItemService itemService) {
+                                                    @Autowired IItemService itemService,
+                                                    @Autowired IUserCreator userCreator,
+                                                    @Autowired IUserXPAdder userXPAdder) {
         super(persistentEventRepository, eventStatusRepository, timeService);
         this.itemService = itemService;
+        this.userCreator = userCreator;
+        this.userXPAdder = userXPAdder;
     }
 
     @Override
@@ -47,5 +58,7 @@ public class InternalSubmissionCompletedEventListener extends AbstractInternalLi
     protected void doProcess(PersistentSubmissionCompletedEvent persistentEvent) throws TransientEventListenerException, NonTransientEventListenerException {
         UUID userId = persistentEvent.getUserId();
         itemService.submissionReward(userId);
+        final UserEntity userEntity = this.userCreator.fetchOrCreate(persistentEvent.getUserId());
+        this.userXPAdder.add(userEntity, IUserXPAdder.Cause.SUBMISSION_COMPLETED);
     }
 }
